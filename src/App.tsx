239940b21dad4dc380cd25399500f9e0e2fb2bb3
Vonglory176 <SkylerGConley@gmail.com>
2025-01-16 15:34:47 -0800

    Built a rough task-viewer modal (Header Only)

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
 import React from 'react';
+import './index.css';
 import './App.css';
-import { Button } from './components/ui/button';
+
+import IssueModal from './modals/IssueModal';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App h-screen bg-background dark">
       <h1 className='text-3xl font-bold'>Taskcord</h1>
-      <Button>Click me</Button>
+      
+      <IssueModal />
     </div>
   );
 }
 
 export default App;
+
+/*
+
+BUGS ---------------------------------------------------------------------------
+
+
+
+TODO ---------------------------------------------------------------------------
+
+
+
+IDEAS ---------------------------------------------------------------------------
+
+
+
+*/
