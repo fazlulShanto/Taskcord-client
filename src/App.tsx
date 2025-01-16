83c23b69d16967cfba298bda6077914ec3db562e
Vonglory176 <SkylerGConley@gmail.com>
2025-01-16 13:03:46 -0800

    Initial project setup (Base React)

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import { Button } from './components/ui/button';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-3xl font-bold'>Taskcord</h1>
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
