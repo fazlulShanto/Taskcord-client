152d34998ea8d5cd4d9916ee01fc9ac2d506d98c
Vonglory176 <SkylerGConley@gmail.com>
2025-01-19 17:01:52 -0800

    Added the default "Sidebar with submenus" ShadCN block

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,17 @@ import './index.css';
 import './App.css';
 
 import TaskModal from './modals/TaskModal';
+import AppSideBar from './components/AppSideBar';
+import { SidebarProvider } from './components/ui/sidebar';
 
 function App() {
   return (
     <div className="App h-screen bg-background dark">
       <h1 className='text-3xl font-bold'>Taskcord</h1>
+
+      <SidebarProvider>
+        <AppSideBar />
+      </SidebarProvider>
       
       <TaskModal />
     </div>
